efa881d049562640c464ab033d3167bcc8aaf572
neelbansal <62473674+neelbansal@users.noreply.github.com>
2021-04-24 13:30:52 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var count = 0;
+
+function add() 
+{
+    count += 1;
+    document.getElementById('counter').innerHTML = count;
+}
+
+function lower()
+ {
+    count -= 1;
+    document.getElementById('counter').innerHTML = count;
+}
